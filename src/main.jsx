3aa2810c3ef404ea6293729f8f7c9c1b3b660be4
Tanvi9929 <tanvigpt205@gmail.com>
2025-07-14 23:51:50 +0530

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,17 @@ import ReactDom from 'react-dom/client'
 import App from './App.jsx'
 import Navbar from './component/Navbar.jsx'
 import Login from './component/login.jsx'
+import './index.css'
+import HeroSection from './component/Herosection.jsx'
 
 ReactDom.createRoot(document.getElementById('root')).render(
   
     <>
     {/* <App/> */}
-    <Navbar/>
-    {/* <Login/> */}
-    <div className="bg-sky-300"><h1 className='underline'>hello</h1></div>
+    {/* <Navbar/> */}
+    <Login/>
+    <HeroSection/>
+
     </>
 
   
